570595702b877e2a1bc06cb3e94e04d7769df238
suraj maurya <surajmaurya450@gmail.com>
2021-10-01 08:10:21 +0530

    Classes added for plans

---
+++ classes/util/SusbcriptionsPlans.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Class deals with Subscription plans.
+ */
+class SusbcriptionsPlans
+{
+	
+	private $db;
+	function __construct() {
+		global $DB;
+		$this->db = $DB;
+	}
+
+	public function add_subcrption_plan() {
+
+	}
+
+	public function get_subscription_plan(){
+
+	}
+
+	public function update_subscription_plan(){
+
+	}
+	
+
+}
